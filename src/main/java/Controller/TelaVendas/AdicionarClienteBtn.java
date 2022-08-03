0e4eb34ea2bc068e905b46439a8fd63537a8c60b
Yan-Fonseca <66083422+Yan-Fonseca@users.noreply.github.com>
2022-08-03 17:17:21 -0300

    Edição do botão Adicionar

--- src/main/java/Controller/TelaVendas/AdicionarClienteBtn.java
+++ src/main/java/Controller/TelaVendas/AdicionarClienteBtn.java
@@ -5,10 +5,15 @@
  */
 package Controller.TelaVendas;
 
+import Model.client.Cliente;
+import TratamentoErro.CpfException;
 import View.CadastrarCliente;
 import View.TelaVendas;
+import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.List;
+import javax.swing.JLabel;
 
 /**
  *
@@ -25,8 +30,34 @@ public class AdicionarClienteBtn implements ActionListener {
         
     @Override
     public void actionPerformed(ActionEvent e) {
-        telaCadastrarCliente = new CadastrarCliente(tela);
-        telaCadastrarCliente.montaTela();
+        //telaCadastrarCliente = new CadastrarCliente(tela);
+        //telaCadastrarCliente.montaTela();
+        String nome = this.tela.getCadastroClienteNome().getText();
+        String cpf = this.tela.getCadastroClienteCpf().getText();
+        try {
+            cpfContemLetras(cpf);
+            cpfUsado(cpf, this.tela.getClienteList().getClientesCadastrados());
+            Cliente cliente = new Cliente(nome,cpf);
+            this.tela.getClienteList().getClientesCadastrados().add(cliente);
+            this.tela.getCadastroClienteCpf().setBackground(Color.WHITE);
+            
+            this.tela.add(new JLabel("Cliente Cadastrado!"));
+            
+            
+        } catch (CpfException err) {
+            this.tela.getCadastroClienteCpf().setBackground(new Color(254, 57, 57));
+        }
     }
-    
+    private void cpfContemLetras(String t) throws NumberFormatException {
+        // passado para double para caber todos os números
+        Double.parseDouble(t);
+    }
+
+    private void cpfUsado(String t, List<Cliente> lista) throws CpfException {
+        for (Cliente f : lista) {
+            if (f.getCpf().equals(t)) {
+                throw new CpfException("CPF já está em uso!!");
+            }
+        }
+    }
 }
